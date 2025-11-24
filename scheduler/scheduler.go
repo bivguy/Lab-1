9430b6328ad8985ed282b0bf6d9fb41a5e2e72b1
bivguy <bivansh@gmail.com>
2025-11-24 14:43:25 -0600

    somehow the correctness dropped

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -166,7 +166,7 @@ func (s *scheduler) Schedule() []*operationBlock {
 			heap.Push(&ready, skippedNode)
 		}
 		cycle += 1
-		// if cycle > 110 {
+		// if cycle > 50 {
 		// 	return nil
 		// }
 		if DEBUG_SCHEDULING {
@@ -182,14 +182,6 @@ func (s *scheduler) Schedule() []*operationBlock {
 				fmt.Println("deleted the ops of length ", len(retiredOps))
 			}
 		}
-
-		// if there are no retiredOps then we must add a NOP
-		// for len(retiredOps) < 2 {
-		// 	if DEBUG_SCHEDULING {
-		// 		fmt.Println("adding nop at cycle ", cycle)
-		// 	}
-		// 	retiredOps = append(retiredOps, notOp)
-		// }
 
 		// remove the active ops
 		delete(active, cycle)
@@ -225,6 +217,43 @@ func (s *scheduler) Schedule() []*operationBlock {
 				}
 			}
 		}
+
+		// go through each multicycle op in active and see if there are any early releases we can do
+		// TODO: this part can be optimized
+		// for _, ops := range active {
+		// 	for _, dn := range ops {
+		// 		// examine each load and store
+		// 		opCode := dn.Op.Opcode
+		// 		if opCode == "load" || opCode == "store" {
+		// 			// check ops that depend on this current operation an early release
+		// 			for _, edge := range dn.ReverseEdges {
+		// 				// skip all nodes that aren't connected by a serial edge
+		// 				if edge.Type != m.SERIALIZATION {
+		// 					continue
+		// 				}
+		// 				// an early release candidate
+		// 				candidate := edge.To
+		// 				// now we must check that all of this node's other dependencies are also satisfied
+		// 				valid := true
+		// 				for _, candidateEdge := range candidate.Edges {
+		// 					// if the node connected via serial edge is not ready or if the node connected by other edge is not retired, we can't do an early release
+		// 					if (candidateEdge.Type == m.SERIALIZATION && candidateEdge.To.Status == NOT_READY) ||
+		// 						(candidateEdge.Type != m.SERIALIZATION && candidateEdge.To.Status != RETIRED) {
+		// 						valid = false
+		// 						break
+		// 					}
+		// 				}
+
+		// 				// this node can be set as ready via early release
+		// 				if valid {
+		// 					heap.Push(&ready, candidate)
+		// 					candidate.Status = READY
+		// 				}
+		// 			}
+		// 		}
+
+		// 	}
+		// }
 	}
 
 	if DEBUG_SCHEDULING {
